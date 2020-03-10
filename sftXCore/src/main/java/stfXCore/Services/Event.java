dbc018f864db4aa3b02d49579114fd80b86430cc
EdgarACarneiro <edgarcarneiro1306@gmail.com>
2020-03-10 18:00:52 +0000

    Raw rotation threshold analysis: needs refactor after reunion

---
+++ sftXCore/src/main/java/stfXCore/Services/Event.java
@@ -0,0 +1,29 @@
+package stfXCore.Services;
+
+public class Event {
+
+    public enum ThresholdTrigger {
+        DELTA,
+        DIRECTED_ACC,
+        ABSOLUTE_ACC
+    }
+
+    public enum Transformation {
+        TRANSLATION,
+        ROTATION,
+        UNIFORM_SCALE
+    }
+
+    private ThresholdTrigger trigger;
+
+    private Transformation type;
+
+    private float triggerValue;
+
+    // TODO: float or Generic type T?
+    Event(ThresholdTrigger trigger, Transformation type, float triggerValue) {
+        this.trigger = trigger;
+        this.type = type;
+        this.triggerValue = triggerValue;
+    }
+}
--- sftXCore/src/main/java/stfXCore/Services/EventsFilter.java
+++ sftXCore/src/main/java/stfXCore/Services/EventsFilter.java
@@ -16,10 +16,54 @@ public class EventsFilter {
      * @param transformations The array to be processed, containing the the transformation values
      * @param threshold       the threshold to use
      */
-    private static void filterThreshold(List<Float> transformations, GenericThreshold<Float> threshold) {
-        // TODO: X and Y são processados individualmente
-        System.out.print(transformations);
-        System.out.print(threshold);
+    // TODO: Refactor into several functions
+    private static ArrayList<Event> filterThreshold(List<Float> transformations, GenericThreshold<Float> threshold) {
+        ArrayList<Event> eventsOfInterest = new ArrayList<>();
+
+        // TODO: Pair<initialState, float>
+        float accDirected = 0;
+        // TODO:  as enum -> 0 for no direction, 1 for positive, -1 for negative
+        int direction = 0;
+        float accAbsolute = 0;
+
+        //TODO: How to manage simultaneous thresholds of a transformation?
+        for (Float transformation : transformations) {
+            if (transformation >= threshold.getDelta()) {
+                eventsOfInterest.add(
+                        new Event(Event.ThresholdTrigger.DELTA,
+                                Event.Transformation.TRANSLATION,
+                                transformation));
+            }
+
+            // TODO: Change with enum
+            // If direction are different from current, reset
+            if (!((direction == 1 && transformation >= 0) ||
+                    (direction == -1 && transformation < 0))) {
+                direction = transformation >= 0? 1 : -1;
+                accDirected = 0;
+            }
+
+            accDirected += Math.abs(transformation);
+
+            if (accDirected >= threshold.getDirectedAcc()) {
+                eventsOfInterest.add(
+                        new Event(Event.ThresholdTrigger.DIRECTED_ACC,
+                                Event.Transformation.TRANSLATION,
+                                accDirected));
+                accDirected = 0;
+            }
+
+            accAbsolute += Math.abs(transformation);
+            if (accAbsolute >= threshold.getAbsoluteAcc()) {
+                eventsOfInterest.add(
+                        new Event(Event.ThresholdTrigger.ABSOLUTE_ACC,
+                                Event.Transformation.TRANSLATION,
+                                accAbsolute));
+                accAbsolute = 0;
+            }
+        }
+
+        return eventsOfInterest;
     }
 
     public static void filter(@NotNull ArrayList<RigidTransformation> rigidTransformations, @NotNull Thresholds thresholds) {
@@ -28,9 +72,9 @@ public class EventsFilter {
         filterThreshold(
                 rigidTransformations.stream().map(rt -> rt.getRotation()).collect(Collectors.toList()),
                 thresholds.getRotation());
-        filterThreshold(
-                rigidTransformations.stream().map(rt -> rt.getTranslation()).collect(Collectors.toList()),
-                thresholds.getRotation());
+//        filterThreshold(
+//                rigidTransformations.stream().map(rt -> rt.getTranslation()[0]).collect(Collectors.toList()),
+//                thresholds.getRotation()[0]);
 
 
         // What is the return type?
