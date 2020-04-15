417929ba9e7288a05f7e4fe729f39edbb5d9009a
EdgarACarneiro <edgarcarneiro1306@gmail.com>
2020-04-15 16:30:25 +0100

    Minor fix to tests

--- stfXCore/src/test/java/stfXCore/GetMetadataTests.java
+++ stfXCore/src/test/java/stfXCore/GetMetadataTests.java
@@ -18,24 +18,22 @@ public class GetMetadataTests extends MockTemplate {
     @Test
     public void testGetMetadata(@Autowired MockMvc mvc) throws Exception {
         mockCPD();
-        mvc.perform(post("/storyboard")
+        String id = mvc.perform(post("/storyboard")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content("{\"dataset\": [[[49.44874674652582, 49.44955690588778], [53.43067605954686, 49.44955600296501], [53.4346053725679, 53.427555100042234], [51.43713990926295, 55.402981930429604], [49.44320808526126, 53.42922779467332], [49.44874674652582, 49.44955690588778]], [[50.0, 50.0], [54.0, 50.0], [54.0, 54.0], [52.0, 56.0], [50.0, 54.0], [50.0, 50.0]]], \"metadata\": {\"timePeriod\": 3, \"name\": \"DatasetName\"}}"))
-                .andExpect(status().isOk());
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
 
-        mvc.perform(get("/storyboard/metadata/3")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content("{}"))
+        mvc.perform(get("/storyboard/metadata/" + id))
                 .andExpect(status().isOk())
                 .andExpect(content().string("{\"timePeriod\":3.0,\"name\":\"DatasetName\"}"));
     }
 
     @Test
     public void testStoryboardNotFound(@Autowired MockMvc mvc) throws Exception {
-        mvc.perform(get("/storyboard/metadata/3")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content("{}"))
+        mvc.perform(get("/storyboard/metadata/10"))
                 .andExpect(status().isNotFound())
-                .andExpect(content().string("Could not find storyboard 3"));
+                .andExpect(content().string("Could not find storyboard 10"));
     }
 }
