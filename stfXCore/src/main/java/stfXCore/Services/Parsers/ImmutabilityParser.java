d178f1a855a96d0a68c669bb28ce3845a3f93602
EdgarACarneiro <edgarcarneiro1306@gmail.com>
2020-04-19 03:39:21 +0100

    Added logic for immutability parser

---
+++ stfXCore/src/main/java/stfXCore/Services/Parsers/ImmutabilityParser.java
@@ -0,0 +1,67 @@
+package stfXCore.Services.Parsers;
+
+import stfXCore.Models.Storyboard.Snapshot;
+import stfXCore.Models.Storyboard.Transformations.RigidTransformation;
+import stfXCore.Services.Events.Event;
+import stfXCore.Utils.Pair;
+
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static stfXCore.Services.Events.Event.ThresholdTrigger.ABSOLUTE_ACC;
+
+public class ImmutabilityParser {
+
+    Long counter;
+
+    ArrayList<Pair<Long, Long>> representations;
+
+    private void resetCounters() {
+        counter = 0L;
+        representations = new ArrayList<>();
+    }
+
+    protected ArrayList<Event<?>> filterThreshold(List<Pair<Snapshot, Boolean>> transformations, Long threshold) {
+        ArrayList<Event<?>> eventsOfInterest = new ArrayList<>();
+        resetCounters();
+
+        if (threshold != null) {
+            for (Pair<Snapshot, Boolean> transformation : transformations) {
+                if (transformation.getSecond()) {
+                    Snapshot s = transformation.getFirst();
+
+                    if (representations.size() == 0)
+                        representations.add(new Pair<Long, Long>(s.getX().getTimestamp(), 0L));
+
+                    counter += s.getY().getTimestamp() - s.getX().getTimestamp();
+                    representations.add(new Pair<Long, Long>(s.getY().getTimestamp(), counter));
+                } else {
+                    resetCounters();
+                    continue;
+                }
+
+                if (counter >= threshold) {
+                    eventsOfInterest.add(
+                            new Event<>(ABSOLUTE_ACC, IMMUTABILITY)
+                                    .setValues(representations));
+                    resetCounters();
+                }
+            }
+        }
+
+        return eventsOfInterest;
+    }
+
+    ImmutabilityParser() {
+    }
+
+    public ArrayList<Event<?>> parse(@NotNull ArrayList<Pair<Snapshot, RigidTransformation>> rigidTransformations, Long threshold) {
+        return filterThreshold(
+                rigidTransformations.stream().map(
+                        pair -> new Pair<>(pair.getFirst(), pair.getSecond().isNull()))
+                        .collect(Collectors.toList()),
+                threshold);
+    }
+}
